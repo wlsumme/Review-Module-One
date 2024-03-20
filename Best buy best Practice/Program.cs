95e4e38324390c069de0f3dbe913c3d221cc1c2e
William Summe <wlsumme@gmail.com>
2024-03-20 10:00:36 -0500

    Dapper

---
+++ Best buy best Practice/Program.cs
@@ -0,0 +1,43 @@
+﻿using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Net.WebSockets;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+#region
+//var departmentRepo = new DapperDepartmentRepository(conn);
+
+//var departments = departmentRepo.GetAllDepartments();
+
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+#endregion
+
+var productRepository = new DapperProductRepository(conn);
+
+var products = productRepository.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
+    Console.WriteLine();
+}
